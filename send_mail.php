e76c0db1af51611fadba34dfcefd0871cf72a5f8
ParkerDesigns <32904116+ParkerDesigns@users.noreply.github.com>
2017-11-03 10:05:42 -0400

    Update form

--- send_mail.php
+++ send_mail.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <?php
 /*
 This first bit sets the email address that you want the form to be submitted to.
